bfa3ac3965ca2496f79d8687d6064f3a488606c7
nur <nurz.mail@gmail.com>
2020-06-24 08:16:25 +0700

    upload repayment

--- application/views/transaction/repayment/_upload.php
+++ application/views/transaction/repayment/_upload.php
@@ -4,7 +4,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Upload Unit Daily Cash</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Upload Repayment</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 </button>
             </div>
