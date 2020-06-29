d7c9d0b7763fb71b306e435baf1d23c4dc8dc669
bagusAditiaSetiawan <bagus.aditia.setiwan@gmail.com>
2020-06-29 14:56:56 +0700

    add employee datamater

---
+++ application/views/datamaster/employees/js.php
@@ -0,0 +1,336 @@
+<script>
+//globals
+var datatable;
+var AlertUtil;
+var createForm;
+var editForm;
+
+function initDTEvents(){
+    $(".btn_delete").on("click",function(){
+        var targetId = $(this).data("id");
+        //alert(targetId);
+        swal.fire({
+            title: 'Anda Yakin?',
+            text: "Akan menghapus data ini",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Ya, Hapus'
+        }).then(function(result) {
+            if (result.value) {
+                KTApp.blockPage();
+                $.ajax({
+                    type : 'GET',
+                    url : "<?php echo base_url("api/datamaster/employees/delete/"); ?>"+targetId,
+                    dataType : "json",
+                    success : function(data,status){
+                        KTApp.unblockPage();
+                        if(data.status == true){
+                            datatable.reload();
+                            AlertUtil.showSuccess(data.message,5000);
+                        }else{
+                            AlertUtil.showFailed(data.message);
+                        }
+                    },
+                    error: function (jqXHR, textStatus, errorThrown){
+                        KTApp.unblockPage();
+                        AlertUtil.showFailed("Cannot communicate with server please check your internet connection");
+                    }
+                });
+            }
+        });
+    });
+
+    $(".btn_edit").on("click",function(){
+        var targetId = $(this).data("id");
+        KTApp.blockPage();
+        $.ajax({
+            type : 'GET',
+            url : "<?php echo base_url("api/datamaster/employees/show/"); ?>"+targetId,
+            dataType : "json",
+            success : function(response,status){
+                KTApp.unblockPage();
+                console.log(response.data);
+                if(response.status == true){
+                    //populate form
+					$('#modal_add').find('[name="id"]').val(response.data.id);
+					$('#modal_add').find('[name="fullname"]').val(response.data.fullname);
+					$('#modal_add').find('[name="id_unit"]').val(response.data.id_unit);
+					$('#modal_add').find('[name="nik"]').val(response.data.nik);
+					$('#modal_add').find('[name="birth_place"]').val(response.data.birth_place);
+					$('#modal_add').find('[name="birth_date"]').val(response.data.birth_date);
+					$('#modal_add').find('[name="username"]').val(response.data.username);
+					$('#modal_add').find('[name="gender"]').val(response.data.gender);
+					$('#modal_add').find('[name="mobile"]').val(response.data.mobile);
+					$('#modal_add').find('[name="marital"]').val(response.data.marital);
+					$('#modal_add').find('[name="blood_group"]').val(response.data.blood_group);
+					$('#modal_add').find('[name="address"]').val(response.data.address);
+					$('#modal_add').find('[name="position"]').val(response.data.position);
+					$('#modal_add').find('[name="id_level"]').val(response.data.id_level);
+                    $('#modal_add').modal('show');
+                }else{
+                    AlertUtil.showFailed(data.message);
+                    $('#modal_add').modal('show');
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown){
+                KTApp.unblockPage();
+                AlertUtil.showFailed("Cannot communicate with server please check your internet connection");
+            }
+        });
+    });
+}
+
+
+function initDataTable(){
+    var option = {
+        data: {
+            type: 'remote',
+            source: {
+              read: {
+                url: '<?php echo base_url("api/datamaster/employees"); ?>',
+                map: function(raw) {
+                  // sample data mapping
+                  var dataSet = raw;
+                  if (typeof raw.data !== 'undefined') {
+                    dataSet = raw.data;
+                  }
+                  return dataSet;
+                },
+              },
+            },
+            serverPaging: true,
+            serverFiltering: true,
+            serverSorting: true,
+            saveState : {cookie: false,webstorage: false},
+          },
+          sortable: true,
+          pagination: true,
+          search: {
+            input: $('#generalSearch'),
+          },
+          columns: [
+            {
+                field: 'id',
+                title: 'ID',
+                sortable: 'asc',
+                width:60,
+                textAlign: 'center',
+            },
+			{
+				field: 'name',
+				title: 'Unit',
+				sortable: 'asc',
+				textAlign: 'left',
+			},
+            {
+                field: 'nik',
+                title: 'Nik',
+                sortable: 'asc',
+                textAlign: 'left',
+            },
+            {
+                field: 'fullname',
+                title: 'Nama',
+                sortable: 'asc',
+                textAlign: 'left',
+            },
+			{
+				field: 'birthday',
+				title: 'Tempat Tanggal Lahir',
+				sortable: 'asc',
+				textAlign: 'left',
+				template:function (row) {
+					return row.birth_place+', '+row.birth_place
+				}
+			},
+			{
+				field: 'gender',
+				title: 'Jenis Kelamin',
+				sortable: 'asc',
+				textAlign: 'left',
+			},
+			{
+				field: 'mobile',
+				title: 'Mobile',
+				sortable: 'asc',
+				textAlign: 'left',
+			},
+			{
+				field: 'martital',
+				title: 'Status Kawin',
+				sortable: 'asc',
+				textAlign: 'left',
+			},
+			{
+				field: 'blood_group',
+				title: 'Golongan Darah',
+				sortable: 'asc',
+				textAlign: 'left',
+			},
+			{
+				field: 'address',
+				title: 'Alamat',
+				sortable: 'asc',
+				textAlign: 'left',
+			},
+			{
+				field: 'position',
+				title: 'Jabatan',
+				sortable: 'asc',
+				textAlign: 'left',
+			},
+            {
+                field: 'action',
+                title: 'Action',
+                sortable: false,
+                width: 100,
+                overflow: 'visible',
+                textAlign: 'center',
+                autoHide: false,
+                template: function (row) {
+                    var result ="";
+                        result = result + '<span data-id="' + row.id + '" href="#" class="btn btn-sm btn-clean btn-icon btn-icon-md btn_edit" title="Edit" ><i class="flaticon-edit-1" style="cursor:pointer;"></i></span>';
+                        result = result + '<span data-id="' + row.id + '" href="#" class="btn btn-sm btn-clean btn-icon btn-icon-md btn_delete" title="Delete" ><i class="flaticon2-trash" style="cursor:pointer;"></i></span>';
+                    return result;
+                }
+            }
+          ],
+          layout:{
+            header:true
+          }
+    }
+    datatable = $('#kt_datatable').KTDatatable(option);
+    datatable.on("kt-datatable--on-layout-updated",function(){
+        initDTEvents();
+    })
+}
+
+function initAlert(){
+    AlertUtil = {
+        showSuccess : function(message,timeout){
+            $("#success_message").html(message);
+            if(timeout != undefined){
+                setTimeout(function(){
+                    $("#success_alert_dismiss").trigger("click");
+                },timeout)
+            }
+            $("#success_alert").show();
+            KTUtil.scrollTop();
+        },
+        hideSuccess : function(){
+            $("#success_alert_dismiss").trigger("click");
+        },
+        showFailed : function(message,timeout){
+            $("#failed_message").html(message);
+            if(timeout != undefined){
+                setTimeout(function(){
+                    $("#failed_alert_dismiss").trigger("click");
+                },timeout)
+            }
+            $("#failed_alert").show();
+            KTUtil.scrollTop();
+        },
+        hideFailed : function(){
+            $("#failed_alert_dismiss").trigger("click");
+        },
+        showFailedDialogAdd : function(message,timeout){
+            $("#failed_message_add").html(message);
+            if(timeout != undefined){
+                setTimeout(function(){
+                    $("#failed_alert_dismiss_add").trigger("click");
+                },timeout)
+            }
+            $("#failed_alert_add").show();
+        },
+        hideSuccessDialogAdd : function(){
+            $("#failed_alert_dismiss_add").trigger("click");
+        },
+        showFailedDialogEdit : function(message,timeout){
+            $("#failed_message_edit").html(message);
+            if(timeout != undefined){
+                setTimeout(function(){
+                    $("#failed_alert_dismiss_edit").trigger("click");
+                },timeout)
+            }
+            $("#failed_alert_edit").show();
+        },
+        hideSuccessDialogAdd : function(){
+            $("#failed_alert_dismiss_edit").trigger("click");
+        }
+    }
+    $("#failed_alert_dismiss").on("click",function(){
+        $("#failed_alert").hide();
+    })
+    $("#success_alert_dismiss").on("click",function(){
+        $("#success_alert").hide();
+    })
+    $("#failed_alert_dismiss_add").on("click",function(){
+        $("#failed_alert_add").hide();
+    })
+    $("#failed_alert_dismiss_edit").on("click",function(){
+        $("#failed_alert_edit").hide();
+    })
+}
+
+
+    //events
+    $("#input-form").on("submit",function(e){
+    	e.preventDefault();
+    	var id = $('[name="id"]').val();
+    	var url;
+    	if(id){
+    		url = "<?php echo base_url("api/datamaster/employees/update"); ?>";
+		}else{
+    		url = "<?php echo base_url("api/datamaster/employees/insert"); ?>";
+		}
+        $.ajax({
+            type : 'POST',
+            url : url,
+            data : $(this).serialize(),
+            dataType : "json",
+            success : function(data,status){
+				$('#modal_add').find('[name="id"]').val("");
+				$('#modal_add').find('[name="name"]').val("");
+				$('#modal_add').find('[name="id_parent"]').val("");
+                KTApp.unblock('#modal_add .modal-content');
+                if(data.status == true){
+                    datatable.reload();
+                    $('#modal_add').modal('hide');
+                    AlertUtil.showSuccess(data.message,5000);
+                }else{
+                    AlertUtil.showFailedDialogAdd(data.message);
+                }
+                getMenu();
+            },
+            error: function (jqXHR, textStatus, errorThrown){
+                KTApp.unblock('#modal_add .modal-content');
+                AlertUtil.showFailedDialogAdd("Cannot communicate with server please check your internet connection");
+            }
+        });
+    });
+
+	function getMenu(){
+		$.ajax({
+			type : 'POST',
+			url : '<?php echo base_url('api/datamaster/employees');?>',
+			data : $(this).serialize(),
+			dataType : "json",
+			success : function(response,status){
+				$('[name="id_parent"]').find('option').remove();
+				$('[name="id_parent"]').append('<option value="0">--Sebagai Menu Utama--</option>');
+				$.each(response.data, function (index, data) {
+					$('[name="id_parent"]').append('<option value="'+data.id+'">'+data.name+'</option>')
+				});
+			},
+		});
+	}
+
+
+
+jQuery(document).ready(function() {
+    initDataTable();
+    initAlert();
+    getMenu();
+});
+
+</script>
--- application/views/temp/MenuBar.php
+++ application/views/temp/MenuBar.php
@@ -9,15 +9,21 @@
                     <a  href="javascript:;" class="kt-menu__link kt-menu__toggle"><span class="kt-menu__link-text">Dashboard</span><i class="kt-menu__ver-arrow la la-angle-right"></i></a>
                         <div class="kt-menu__submenu kt-menu__submenu--classic kt-menu__submenu--left">
                             <ul class="kt-menu__subnav">
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                                <a href="<?php echo base_url('dashboards/pusat'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Dashboard Pusat</span></a>
-                            </li>
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                                <a  href="<?php echo base_url('dashboards/area'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Dashboard Area</span></a>
-                            </li>
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                            <a  href="<?php echo base_url('dashboards/units'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Dashboard Units</span></a>
-                            </li>
+							<?php if(read_access('pusat')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a href="<?php echo base_url('dashboards/pusat'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Dashboard Pusat</span></a>
+								</li>
+							<?php endif;?>
+							<?php if(read_access('area')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a  href="<?php echo base_url('dashboards/area'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Dashboard Area</span></a>
+								</li>
+							<?php endif;?>
+							<?php if(read_access('units')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a  href="<?php echo base_url('dashboards/units'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Dashboard Units</span></a>
+								</li>
+							<?php endif;?>
                             </ul>
                         </div>
                     </li>
@@ -25,15 +31,21 @@
                     <a  href="javascript:;" class="kt-menu__link kt-menu__toggle"><span class="kt-menu__link-text">Site Setting</span><i class="kt-menu__ver-arrow la la-angle-right"></i></a>
                         <div class="kt-menu__submenu kt-menu__submenu--classic kt-menu__submenu--left">
                             <ul class="kt-menu__subnav">
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                                <a href="<?php echo base_url('site-settings/menu');?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Menu</span></a>
-                            </li>
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                                <a  href="<?php echo base_url('site-settings/levels');?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Level</span></a>
-                            </li>
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                                <a  href="<?php echo base_url('site-settings/privileges');?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Hak Akses</span></a>
-                            </li>
+							<?php if(read_access('menu')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a href="<?php echo base_url('site-settings/menu');?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Menu</span></a>
+								</li>
+							<?php endif;?>
+							<?php if(read_access('levels')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a  href="<?php echo base_url('site-settings/levels');?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Level</span></a>
+								</li>
+							<?php endif;?>
+							<?php if(read_access('privileges')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a  href="<?php echo base_url('site-settings/privileges');?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Hak Akses</span></a>
+								</li>
+							<?php endif;?>
                             </ul>
                         </div>
                     </li>
@@ -41,21 +53,31 @@
                     <a  href="javascript:;" class="kt-menu__link kt-menu__toggle"><span class="kt-menu__link-text">Data Master</span><i class="kt-menu__ver-arrow la la-angle-right"></i></a>
                         <div class="kt-menu__submenu kt-menu__submenu--classic kt-menu__submenu--left">
                             <ul class="kt-menu__subnav">
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                                <a href="demo2/index.html" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Pegawai</span></a>
-                            </li>
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                            <a  href="<?php echo base_url('datamaster/customers');?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Nasabah</span></a>
-                            </li>
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                                <a  href="<?php echo base_url('datamaster/areas'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Area</span></a>
-                            </li>
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                            <a  href="<?php echo base_url('datamaster/units'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Unit</span></a>
-                            </li>
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                            <a  href="<?php echo base_url('datamaster/unitstarget'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Target Unit</span></a>
-                            </li>
+							<?php if(read_access('employees')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a href="<?php echo base_url('datamaster/employees')?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Pegawai</span></a>
+								</li>
+							<?php endif;?>
+							<?php if(read_access('customers')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a  href="<?php echo base_url('datamaster/customers');?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Nasabah</span></a>
+								</li>
+							<?php endif;?>
+							<?php if(read_access('areas')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a  href="<?php echo base_url('datamaster/areas'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Area</span></a>
+								</li>
+							<?php endif;?>
+							<?php if(read_access('units')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a  href="<?php echo base_url('datamaster/units'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Unit</span></a>
+								</li>
+							<?php endif;?>
+							<?php if(read_access('unitstarget')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a  href="<?php echo base_url('datamaster/unitstarget'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Target Unit</span></a>
+								</li>
+							<?php endif;?>
                             <li class="kt-menu__item "  aria-haspopup="true">
                             <a  href="demo2/dashboards/aside.html" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">STLE</span></a>
                             </li>
@@ -67,27 +89,36 @@
                     <a  href="javascript:;" class="kt-menu__link kt-menu__toggle"><span class="kt-menu__link-text">Transaksi</span><i class="kt-menu__ver-arrow la la-angle-right"></i></a>
                         <div class="kt-menu__submenu kt-menu__submenu--classic kt-menu__submenu--left">
                             <ul class="kt-menu__subnav">
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                                <a href="<?php echo base_url('transactions/unitsdailycash'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Harian Unit</span></a>
-                            </li>
-							<li class="kt-menu__item "  aria-haspopup="true">
-								<a  href="<?php echo base_url('transactions/regularpawns'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Gadai Reguler</span></a>
-							</li>
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                                <a  href="<?php echo base_url('transactions/regularpawns') ;?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Unit Regular Pawn</span></a>
-                            </li>
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                            <a  href="<?php echo base_url('transactions/mortages');?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Gadai Cicilan</span></a>
-                            </li>
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                            <a  href="<?php echo base_url('transactions/repayment'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Pelunasan</span></a>
-                            </li>
-                            <li class="kt-menu__item "  aria-haspopup="true">
-                            <a  href="<?php echo base_url('transactions/loaninstallments'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Angsuran Pinjaman Cicilan</span></a>
-                            </li>
-							<li class="kt-menu__item "  aria-haspopup="true">
-								<a  href="<?php echo base_url('transactions/extractall'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Upload Semua</span></a>
-							</li>
+							<?php if(read_access('unitsdailycash')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a href="<?php echo base_url('transactions/unitsdailycash'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Harian Unit</span></a>
+								</li>
+							<?php endif;?>
+							<?php if(read_access('regularpawns')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a  href="<?php echo base_url('transactions/regularpawns'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Gadai Reguler</span></a>
+								</li>
+							<?php endif;?>
+							<?php if(read_access('mortages')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a  href="<?php echo base_url('transactions/mortages');?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Gadai Cicilan</span></a>
+								</li>
+							<?php endif;?>
+							<?php if(read_access('repayment')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a  href="<?php echo base_url('transactions/repayment'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Pelunasan</span></a>
+								</li>
+							<?php endif;?>
+							<?php if(read_access('loaninstallments')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a  href="<?php echo base_url('transactions/loaninstallments'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Angsuran Pinjaman Cicilan</span></a>
+								</li>
+							<?php endif;?>
+							<?php if(read_access('extractall')):?>
+								<li class="kt-menu__item "  aria-haspopup="true">
+									<a  href="<?php echo base_url('transactions/extractall'); ?>" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Upload Semua</span></a>
+								</li>
+							<?php endif;?>
                             </ul>
                         </div>
                     </li>
