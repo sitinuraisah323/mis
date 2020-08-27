e8ce507bd14388d111ab26cba50117f62a538aca
bagusAditiaSetiawan <bagus.aditia.setiwan@gmail.com>
2020-08-27 21:50:30 +0700

    repair unit daialy cash

--- application/controllers/api/transactions/Unitsdailycash.php
+++ application/controllers/api/transactions/Unitsdailycash.php
@@ -424,7 +424,7 @@ class Unitsdailycash extends ApiController
 			$this->saldo->db->where('id_unit', $idUnit);
 		}
 		if($date){
-			$this->saldo->db->where('date >', $date);
+			$this->saldo->db->where('date >=', $date);
 		}
 
 		if($dateStart){
@@ -454,7 +454,7 @@ class Unitsdailycash extends ApiController
 
 		if($get = $this->input->get()){
 			$this->unitsdailycash->db
-				->where('date >', $get['dateStart'])
+				->where('date >=', $get['dateStart'])
 				->where('date <=', $get['dateEnd']);
 			if($get['id_unit']!='all' && $get['id_unit'] != 0){
 				$this->unitsdailycash->db->where('id_unit', $get['id_unit']);
