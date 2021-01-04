50af2e615a30543c05b5a30586652700fbed416f
bagusAditiaSetiawan <bagus.aditia.setiwan@gmail.com>
2021-01-04 09:23:15 +0700

    add script nasabah

--- application/models/CustomersModel.php
+++ application/models/CustomersModel.php
@@ -8,76 +8,50 @@ class CustomersModel extends Master
 
 	public function current($permit, $area)
 	{
-		
-		$customerPerPersion = $this->db
-			->select('count(distinct(customers.id)) as per_person')
+		$getCustomer = $this->db
+			->select('distinct(customers.id), customers.name, no_cif')
 			->from('customers')
 			->join('units_regularpawns','units_regularpawns.id_customer = customers.id')
 			->join('units','units.id = units_regularpawns.id_unit')
 			->where('units.id_area', $area)
 			->where('permit', $permit)
+			->where('status_transaction','N')
 			->get()
-			->row();
-		$transactionBigger = $this->db
-			->select('count(*) as bigger')
-			->from('customers')
-			->join('units_regularpawns','units_regularpawns.id_customer = customers.id')
-			->join('units','units.id = units_regularpawns.id_unit')
-			->where('units.id_area', $area)
-			->where('amount >=', 100000000)
-			->where('permit', $permit)
-			->get()
-			->row();
-		$transactionSmaller = $this->db
-			->select('count(*) as small')
-			->from('customers')
-			->join('units_regularpawns','units_regularpawns.id_customer = customers.id')
-			->join('units','units.id = units_regularpawns.id_unit')
-			->where('units.id_area', $area)
-			->where('amount <', 100000000)
-			->where('permit', $permit)
-			->get()
-			->row();
+			->result();
 
-		$customerPerPersionDetail = $this->db
-			->select('distinct(customers.id)')
-			->from('customers')
-			->join('units_regularpawns','units_regularpawns.id_customer = customers.id')
-			->join('units','units.id = units_regularpawns.id_unit')
-			->where('units.id_area', $area)
-			->where('permit', $permit)
-			->get()
-			->result();
-		$transactionBiggerDetail = $this->db
-			->select('*')
-			->from('customers')
-			->join('units_regularpawns','units_regularpawns.id_customer = customers.id')
-			->join('units','units.id = units_regularpawns.id_unit')
-			->where('units.id_area', $area)
-			->where('amount >=', 100000000)
-			->where('permit', $permit)
-			->get()
-			->result();
-		$transactionSmallerDetail = $this->db
-			->select('*')
-			->from('customers')
-			->join('units_regularpawns','units_regularpawns.id_customer = customers.id')
-			->join('units','units.id = units_regularpawns.id_unit')
-			->where('units.id_area', $area)
-			->where('amount <', 100000000)
-			->where('permit', $permit)
-			->get()
-			->result();
+		
+		$countCustomer = count($getCustomer);
+
+		$bigger = 0;
+		$smaller = 0;
+
+		if($getCustomer){
+			foreach($getCustomer as $customer){
+				$amount = $this->db
+						->select('sum(units_regularpawns.amount) as amount')
+						->from('customers')
+						->join('units_regularpawns','units_regularpawns.id_customer = customers.id')
+						->join('units','units.id = units_regularpawns.id_unit')
+						->where('units.id_area', $area)
+						->where('permit', $permit)
+						->where('status_transaction','N')
+						->where('units_regularpawns.id_customer', $customer->id)
+						->get()
+						->row();
+				if($amount->amount >= 100000000){
+					$bigger++;
+				}else{
+					$smaller++;
+				}
+				
+			}
+		}
+		
 
 		return array(
-			'customer_per_person'	=> $customerPerPersion->per_person,
-			'transaction_bigger'	=> $transactionBigger->bigger,
-			'transaction_smaller'	=> $transactionSmaller->small,
-			'details'	=> array(
-				'customer_per_person'	=> $customerPerPersionDetail,
-				'transaction_bigger'	=> $transactionBiggerDetail,
-				'transaction_smaller'	=> $transactionSmallerDetail,
-			)
+			'customer_per_person'	=> $countCustomer,
+			'transaction_bigger'	=> $bigger,
+			'transaction_smaller'	=> $smaller,
 		);
 	}
 }
