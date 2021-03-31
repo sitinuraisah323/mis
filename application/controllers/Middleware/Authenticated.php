fd3a603e7f175b1dd881dc10f0c258cf10fbcb52
bagusAditiaSetiawan <bagus.aditia.setiwan@gmail.com>
2021-03-31 14:26:25 +0700

    repair os

--- application/controllers/Middleware/Authenticated.php
+++ application/controllers/Middleware/Authenticated.php
@@ -10,19 +10,45 @@ class Authenticated extends Controller
 		if($session['logged_in'] == false){
 			redirect(base_url('login'));
 		}
+		
+		$this->load->model('MenuModel', 'menus');
+		$segments =$this->uri->segment_array();
+		
 		if($privileges = $session['privileges']){
 			if($segments =$this->uri->segment_array()){
 				foreach ($segments as $index => $segment){
-					if(key_exists($index-1,$privileges)){
-						if(key_exists($segment, $privileges[$index-1])){
-							if($privileges[$index-1][$segment] == 'DENIED'){
-								redirect(base_url('denied'));
-							}
-						}
+					$dept = $index-1;
+					if(!$this->checkPrivileges($segment, $dept, $session['user']->id_level)){
+						redirect(base_url('denied'));
 					}
 				}
 			}
 		}
+	}
+
+	public function checkPrivileges($segment, $dept, $idLevel)
+	{
+		$getPrivileges = $this->menus->db
+			->select('levels_privileges.id, levels_privileges.can_access, menus.name')
+			->from('levels_privileges')
+			->join('menus', 'menus.id = levels_privileges.id_menu')
+			->where('levels_privileges.id_level', $idLevel)
+			->where('menus.dept', $dept)
+			->where('menus.name', $segment)
+			->get()
+			->result();
+		$check = true;
+		if($getPrivileges){
+			foreach($getPrivileges as $getPrivilege){
+				if($getPrivilege->can_access === 'WRITE' || $getPrivilege->can_access === 'READ'){
+					return true;
+				}else if($getPrivilege->can_access === 'DENIED'){
+					$check = false;
+				}
+			}
+		}
+		
+		return $check;
 	}
 
 }
